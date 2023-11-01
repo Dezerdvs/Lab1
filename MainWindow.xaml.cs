c4c8fbf7a48f61c0dd6ef1b38ebd97d7c3389cd4
Dezerdvs <makentosh783@gmail.com>
2023-11-01 23:37:30 +0200

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+
+
+namespace Lab1
+{
+    public partial class MainWindow : Window
+    {
+        private Rhombus _rhombus;
+        private int _countCalculate = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+            _rhombus = new Rhombus();
+        }
+
+        private void btnCalculate_Click(object sender, RoutedEventArgs e)
+        {
+            if(double.TryParse(tbSideLength.Text, out double sideLength))
+            {
+                _rhombus.SideLength = sideLength;
+            }
+            else if(tbSideLength.Text.Length == 0)
+            {
+                _rhombus = new Rhombus();
+            }
+            else
+            {
+                MessageBox.Show("Номер недійсний", "Помилка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            MessageBox.Show($"Результат після розрахунків: {_rhombus.CalculateArea()}", "Результат!", MessageBoxButton.OK, MessageBoxImage.Information);
+            _countCalculate++;
+
+        }
+
+        
+    }
+}
---
+++ Rhombus.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    public class Rhombus
+    {
+        private double sideLength;
+        public double SideLength
+        {
+            get
+            {
+                return sideLength;
+            }
+            set
+            {
+                if (sideLength <= 0)
+                {
+                    throw new ArgumentException("Щось пішло не так, довжина має бути більше ніж 0");
+                }
+                sideLength = value;
+            }
+        }
+
+        public Rhombus()
+        {
+            sideLength = 1;
+        }
+
+        public Rhombus(Rhombus rhombus)
+        {
+            this.sideLength = rhombus.sideLength;
+        }
+        public Rhombus(double sideLength)
+        {
+            if(sideLength <= 0)
+            {
+                throw new ArgumentException("Щось пішло не так, довжина має бути більше ніж 0");
+            }
+            this.sideLength = sideLength;
+        }
+
+        public double CalculateArea()
+        {
+            return Math.Pow(sideLength, 2) * Math.Sin(Math.PI / 3);
+        }
+
+    }
+}
